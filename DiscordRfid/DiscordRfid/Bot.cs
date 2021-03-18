aa33c14bc5fd4124de9f606737b9ef434ceb711f
Alija Bobija <abobijax@gmail.com>
2021-03-18 17:25:04 +0100

    Refactoring

--- DiscordRfid/DiscordRfid/Bot.cs
+++ DiscordRfid/DiscordRfid/Bot.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,26 +12,108 @@ namespace DiscordRfid
 
         public DiscordSocketClient Client { get; set; }
 
+        public string Name => Client?.CurrentUser?.Username;
         public SocketGuild Guild => Client.Guilds.ElementAt(0);
         public IRole MasterRole { get; set; }
         public IRole SlaveRole { get; set; }
         public ITextChannel Channel { get; set; }
 
+        public event Action<Exception> ConnectError;
+        public event Action<Exception> EnvironmentCreationError;
+        public event Action Ready;
+
+        public Func<string> TokenProvider { get; set; }
+        public Func<bool> RolesCreationPrompter { get; set; }
+        public Func<bool> ChannelCreationPrompter { get; set; }
+
         protected Bot()
         {
             Client = new DiscordSocketClient();
 
-            Client.GuildAvailable += g =>
+            Client.GuildAvailable += async g =>
             {
                 MasterRole = Guild.Roles.FirstOrDefault(r => r.Name == Constants.MasterRoleName);
                 SlaveRole = Guild.Roles.FirstOrDefault(r => r.Name == Constants.SlaveRoleName);
                 Channel = Guild.TextChannels.FirstOrDefault(c => c.Name == Constants.ChannelName);
 
-                return Extensions.NoopTask();
+                try
+                {
+                    await CheckRolesAsync();
+                    await CheckChannelAsync();
+                    Ready?.Invoke();
+                } catch(Exception ex)
+                {
+                    EnvironmentCreationError?.Invoke(ex);
+                }
             };
         }
 
-        public async Task CreateRolesAsync()
+        public async Task ConnectAsync()
+        {
+            var config = Configuration.Instance;
+            string token = config.Token;
+            bool check = true;
+
+            while (check)
+            {
+                if (token == null)
+                {
+                    if(TokenProvider == null)
+                    {
+                        break;
+                    }
+
+                    token = TokenProvider();
+                }
+
+                try
+                {
+                    await Client.LoginAsync(TokenType.Bot, token);
+                    await Client.StartAsync();
+                    config.Token = token;
+                    check = false;
+                }
+                catch (Exception ex)
+                {
+                    config.Token = token = null;
+                    ConnectError?.Invoke(ex);
+                }
+            }
+        }
+
+        protected async Task CheckRolesAsync()
+        {
+            if (MasterRole == null || SlaveRole == null)
+            {
+                if(! RolesCreationPrompter())
+                {
+                    throw new Exception("Roles are required");
+                }
+
+                await CreateRolesAsync();
+            }
+
+            await SelfAssignMasterRoleAsync();
+        }
+
+        protected async Task CheckChannelAsync()
+        {
+            var bot = Bot.Instance;
+
+            if (bot.Channel == null)
+            {
+                if(! ChannelCreationPrompter())
+                {
+                    throw new Exception($"Textual channel \"{Constants.ChannelName}\" is required");
+                }
+
+                bot.Channel = await bot.Guild.CreateTextChannelAsync(Constants.ChannelName);
+            }
+
+            await bot.SetChannelPermissionsAsync();
+        }
+
+        protected async Task CreateRolesAsync()
         {
             var perms = new GuildPermissions(
                 viewChannel: true,
@@ -51,7 +134,7 @@ namespace DiscordRfid
             }
         }
 
-        public async Task SelfAssignMasterRoleAsync()
+        protected async Task SelfAssignMasterRoleAsync()
         {
             var self = Guild.GetUser(Client.CurrentUser.Id);
             var masterRoleAssigned = self.Roles.FirstOrDefault(r => r.Id == MasterRole.Id) != null;
@@ -62,7 +145,7 @@ namespace DiscordRfid
             }
         }
 
-        public async Task SetChannelPermissionsAsync()
+        protected async Task SetChannelPermissionsAsync()
         {
             var everyoneOw = Channel.PermissionOverwrites.FirstOrDefault(ow =>
                 ow.TargetType == PermissionTarget.Role && ow.TargetId == Guild.EveryoneRole.Id
--- DiscordRfid/DiscordRfid/MainForm.cs
+++ DiscordRfid/DiscordRfid/MainForm.cs
@@ -1,6 +1,4 @@
-﻿using Discord;
-using System;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace DiscordRfid
@@ -16,132 +14,94 @@ namespace DiscordRfid
             InitializeComponent();
             BotName = "";
             ServerName = "";
-            Shown += (o, e) => ConnectAsync().ContinueWithNoop();
-        }
 
-        private async Task ConnectAsync()
-        {
-            var config = Configuration.Instance;
-            string token = config.Token;
-            bool check = true;
+            var bot = Bot.Instance;
 
-            State = "Connecting...";
-            Bot.Instance.Client.Connected += () => OnConnected();
+            bot.TokenProvider = TokenProvider;
+            bot.ConnectError += OnConnectError;
+            bot.RolesCreationPrompter = RolesCreationPrompt;
+            bot.ChannelCreationPrompter = ChannelCreationPrompt;
 
-            while (check)
-            {
-                if (token == null)
-                {
-                    using (var dlg = new TokenForm { Message = "Please enter token of Discord bot" })
-                    {
-                        if (dlg.ShowDialog() != DialogResult.OK)
-                        {
-                            Close();
-                            return;
-                        }
-
-                        token = dlg.Token;
-                    }
-                }
-
-                try
-                {
-                    await Bot.Instance.Client.LoginAsync(TokenType.Bot, token);
-                    await Bot.Instance.Client.StartAsync();
-                    config.Token = token;
-                    check = false;
-                }
-                catch (Exception ex)
-                {
-                    config.Token = token = null;
-                    this.Error(string.Format("Invalid token.{0}{1}{0}{0}Please try again.", Environment.NewLine, ex.Message));
-                }
-            }
-        }
-
-        protected Task OnConnected()
-        {
-            BotName = Bot.Instance.Client.CurrentUser.Username;
-            State = "Checking environment...";
-
-            Bot.Instance.Client.GuildAvailable += g => OnGuildAvailable();
-
-            return Extensions.NoopTask();
-        }
-
-        private Task OnGuildAvailable()
-        {
-            try
-            {
-                ServerName = $"@ {Bot.Instance.Guild.Name}";
-
-                CheckRolesAsync().GetAwaiter().GetResult();
-                CheckChannelAsync().GetAwaiter().GetResult();
-
-                State = "Ready";
-            }
-            catch (Exception ex)
+            bot.EnvironmentCreationError += ex =>
             {
                 this.Error(ex);
                 this.SafeClose();
-            }
+            };
 
-            return Extensions.NoopTask();
+            bot.Client.Connected += () =>
+            {
+                BotName = Bot.Instance.Name;
+                State = "Checking environment...";
+                return Extensions.NoopTask();
+            };
+
+            bot.Client.GuildAvailable += g =>
+            {
+                ServerName = $"@ {Bot.Instance.Guild.Name}";
+                return Extensions.NoopTask();
+            };
+
+            bot.Ready += () => State = "Ready";
+
+            Shown += (o, e) =>
+            {
+                State = "Connecting...";
+                bot.ConnectAsync().ContinueWithNoop();
+            };
         }
 
-        private async Task CheckRolesAsync()
+        private string TokenProvider()
         {
-            var bot = Bot.Instance;
+            string token = null;
 
-            if (bot.MasterRole == null || bot.SlaveRole == null)
+            using (var dlg = new TokenForm { Message = "Please enter token of Discord bot" })
             {
-                var rolesQuestion = $"Discord server \"{bot.Guild.Name}\" does not have required roles which is used for differentiate RFID master and slaves:" +
-                Environment.NewLine;
-
-                if (bot.MasterRole == null)
+                if (dlg.ShowDialog() != DialogResult.OK)
                 {
-                    rolesQuestion += $"{Environment.NewLine}- {Constants.MasterRoleName}";
+                    Close();
                 }
 
-                if (bot.SlaveRole == null)
-                {
-                    rolesQuestion += $"{Environment.NewLine}- {Constants.SlaveRoleName}";
-                }
-
-                rolesQuestion += $"{Environment.NewLine}{Environment.NewLine}" +
-                    "Do you want to create them now?";
-
-                if (this.Question(rolesQuestion) != DialogResult.Yes)
-                {
-                    throw new Exception("Roles are required");
-                }
-
-                await bot.CreateRolesAsync();
+                token = dlg.Token;
             }
 
-            await bot.SelfAssignMasterRoleAsync();
+            return token;
         }
 
-        private async Task CheckChannelAsync()
+        private void OnConnectError(Exception ex)
+        {
+            this.Error(string.Format("{0}{1}{1}Please try again.", ex.Message, Environment.NewLine));
+        }
+
+        private bool RolesCreationPrompt()
         {
             var bot = Bot.Instance;
-            
-            if(bot.Channel == null)
+            var rolesQuestion = $"Discord server \"{bot.Guild.Name}\" does not have required roles which is used for differentiate RFID master and slaves:" +
+                   Environment.NewLine;
+
+            if (bot.MasterRole == null)
             {
-                var question = $"Discord server \"{bot.Guild.Name}\" does not contain textual channel with name \"{Constants.ChannelName}\" which " +
-                    "is used for communication with RFID slaves." +
-                    $"{Environment.NewLine}{Environment.NewLine}" +
-                    "Would you like to create this channel now?";
-
-                if(this.Question(question) != DialogResult.Yes)
-                {
-                    throw new Exception($"Textual channel \"{Constants.ChannelName}\" is required");
-                }
-
-                bot.Channel = await bot.Guild.CreateTextChannelAsync(Constants.ChannelName);
+                rolesQuestion += $"{Environment.NewLine}- {Constants.MasterRoleName}";
             }
 
-            await bot.SetChannelPermissionsAsync();
+            if (bot.SlaveRole == null)
+            {
+                rolesQuestion += $"{Environment.NewLine}- {Constants.SlaveRoleName}";
+            }
+
+            rolesQuestion += $"{Environment.NewLine}{Environment.NewLine}" +
+                "Do you want to create them now?";
+
+            return this.Question(rolesQuestion) == DialogResult.Yes;
+        }
+
+        private bool ChannelCreationPrompt()
+        {
+            var question = $"Discord server \"{Bot.Instance.Guild.Name}\" does not contain textual channel with name \"{Constants.ChannelName}\" which " +
+                "is used for communication with RFID slaves." +
+                $"{Environment.NewLine}{Environment.NewLine}" +
+                "Would you like to create this channel now?";
+
+            return this.Question(question) == DialogResult.Yes;
         }
     }
 }
