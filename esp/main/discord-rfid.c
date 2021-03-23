88bc68d21fdb4c32b0bc6ef61796d5556d362136
abobija <abobijax@gmail.com>
2021-03-23 20:22:55 +0100

    Loading environment and sending package

--- esp/main/discord-rfid.c
+++ esp/main/discord-rfid.c
@@ -1,3 +1,20 @@
+/**
+ * @author  Alija Bobija
+ * @date    Mart, 2021
+ * 
+ * @brief   This code represent ESP-IDF application for ESP32 which role is
+ *          to establish Discord bot which will have the role of the slave in RFID
+ *          attendance system. RFID module MFRC522 is attached on ESP32 and on
+ *          every scanned tag serial number will be published to specific
+ *          Discord guild channel. All this is a part of github "abobija/discord-rfid"
+ *          repository, and there is more information.
+ * 
+ * @note    This is not intended for use in professional use and because of that
+ *          the code lacks memory and other checks in many places
+ * 
+ * @link    https://github.com/abobija/discord-rfid
+ */
+
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
@@ -8,6 +25,9 @@
 #include "estr.h"
 #include "cJSON.h"
 #include "discord.h"
+#include "discord/user.h"
+#include "discord/guild.h"
+#include "discord/channel.h"
 #include "discord/session.h"
 #include "discord/message.h"
 
@@ -39,7 +59,71 @@ static char* package_to_string(package_t* package);
 static void device_free(device_t* device);
 static void package_free(package_t* package);
 
-static discord_handle_t bot;
+static discord_handle_t   bot     = NULL;
+static discord_guild_t*   guild   = NULL;
+static discord_channel_t* channel = NULL;
+
+static void load_environment() {
+    discord_guild_t** guilds = NULL;
+    int guilds_len = 0;
+
+    ESP_LOGI(TAG, "Fetching server info...");
+    if(discord_user_get_my_guilds(bot, &guilds, &guilds_len) != ESP_OK) {
+        ESP_LOGE(TAG, "Fail to get my guilds");
+        goto _restart;
+    }
+
+    guild = guilds[0]; // Suppose that bot is in only one guild
+    if(guilds_len > 1) {
+        ESP_LOGW(TAG, "Bot is in more than one guild. Application will not work properly");
+    }
+
+    ESP_LOGI(TAG, "Bot is in server: %s", guild->name);
+    ESP_LOGI(TAG, "Fetching channel info...");
+
+    discord_channel_t** channels = NULL;
+    int channels_len = 0;
+    if(discord_guild_get_channels(bot, guild, &channels, &channels_len) != ESP_OK) {
+        ESP_LOGE(TAG, "Fail to get channel list");
+        goto _restart;
+    }
+
+    // Search for rfid channel
+    for(int i = 0; i < channels_len; i++) {
+        if(estr_eq(channels[i]->name, CONFIG_DISCORD_CHANNEL_NAME) && channels[i]->type == DISCORD_CHANNEL_GUILD_TEXT) {
+            channel = cu_ctor(discord_channel_t,
+                .id   = channels[i]->id,
+                .type = channels[i]->type,
+                .name = channels[i]->name
+            );
+
+            channels[i]->id = NULL;
+            channels[i]->name = NULL;
+            break;
+        }
+    }
+
+    cu_list_freex(channels, channels_len, discord_channel_free);
+
+    if(channel == NULL) {
+        ESP_LOGE(TAG,
+            "Cannot find channel \"" CONFIG_DISCORD_CHANNEL_NAME "\" on server \"%s\". "
+            "If channel exist than please check if bot have attached correct slave role in server.",
+            guild->name
+        );
+
+        goto _restart;
+    }
+
+    goto _end;
+_restart:
+    ESP_LOGW(TAG, "Restarting in 15 seconds...");
+    vTaskDelay(15000 / portTICK_PERIOD_MS);
+    fflush(stdout);
+    esp_restart();
+_end:
+    return;
+}
 
 static void bot_event_handler(void* handler_arg, esp_event_base_t base, int32_t event_id, void* event_data) {
     discord_event_data_t* data = (discord_event_data_t*) event_data;
@@ -48,7 +132,13 @@ static void bot_event_handler(void* handler_arg, esp_event_base_t base, int32_t 
         case DISCORD_EVENT_CONNECTED: {
                 discord_session_t* session = (discord_session_t*) data->ptr;
                 discord_session_dump_log(ESP_LOGI, TAG, session);
-                rc522_start2();
+                load_environment(); // guild, channel info...
+                if(channel != NULL) {
+                    rc522_start2();
+                    ESP_LOGI(TAG, "Initialized. Listening for RFID tags...");
+                } else {
+                    ESP_LOGW(TAG, "RFID not started. Discord channel not found");
+                }
             }
             break;
         
@@ -87,22 +177,35 @@ void rfid_handler(uint8_t* sn) {
 
     package_t* package = package_create_tag(tag64);
     char* pstr = package_to_string(package);
-    
-    ESP_LOGI(TAG, "%s", pstr);
-    // send package...
+
+    discord_message_t message = {
+        .content = pstr,
+        .channel_id = channel->id
+    };
+
+    ESP_LOGI(TAG, "Sending package to " CONFIG_DISCORD_CHANNEL_NAME " channel...");
+    if(discord_message_send(bot, &message, NULL) != ESP_OK) {
+        ESP_LOGW(TAG, "Fail to send package");
+    } else {
+        ESP_LOGI(TAG, "Package sent");
+    }
 
     free(pstr);
     package_free(package);
 }
 
 void app_main(void) {
+    //esp_log_level_set("*", ESP_LOG_INFO);
+    //esp_log_level_set("DISCORD", ESP_LOG_DEBUG);
+
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect());
 
     discord_config_t cfg = {
-        .intents = DISCORD_INTENT_GUILD_MESSAGES
+        .intents = DISCORD_INTENT_GUILD_MESSAGES,
+        .api_buffer_size = 7 * 1024
     };
 
     bot = discord_create(&cfg);
