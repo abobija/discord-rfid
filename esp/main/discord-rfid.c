562b3013879fca7df8ca5ba5f3d9df8d328b087e
abobija <abobijax@gmail.com>
2021-03-23 16:08:31 +0100

    Package structs

--- esp/main/discord-rfid.c
+++ esp/main/discord-rfid.c
@@ -3,11 +3,39 @@
 #include "protocol_examples_common.h"
 
 #include "rc522.h"
+#include "b64.h"
+#include "cutils.h"
+#include "estr.h"
 #include "discord.h"
 #include "discord/session.h"
 #include "discord/message.h"
 
 static const char* TAG = "discord-rfid";
+
+typedef enum {
+    PACKAGE_UNKNOWN,
+    PACKAGE_TAG,
+    PACKAGE_PING,
+    PACKAGE_PONG
+} package_type_t;
+
+typedef struct {
+    char* name;
+    char* version;
+    uint64_t uptime;
+    uint64_t heap;
+} device_t;
+
+typedef struct {
+    package_type_t type;
+    device_t* device;
+    uint64_t* serial_number;
+} package_t;
+
+static device_t* device_create();
+static package_t* package_create_tag(uint64_t serial_number);
+static void device_free(device_t* device);
+static void package_free(package_t* package);
 
 static discord_handle_t bot;
 
@@ -41,9 +69,25 @@ static void bot_event_handler(void* handler_arg, esp_event_base_t base, int32_t 
 }
 
 void rfid_handler(uint8_t* sn) {
-	ESP_LOGI(TAG, "Tag: %#x %#x %#x %#x %#x", 
-		sn[0], sn[1], sn[2], sn[3], sn[4]
+    discord_gateway_state_t state;
+    discord_get_state(bot, &state);
+
+    if(state != DISCORD_STATE_CONNECTED) {
+        ESP_LOGW(TAG, "Discard RFID Tag. Not connected with Discord");
+        return;
+    }
+
+    uint64_t tag64 = rc522_sn_to_u64(sn);
+
+	ESP_LOGI(TAG, "Tag: %" PRIu64 " (%#x %#x %#x %#x %#x)",
+		tag64, sn[0], sn[1], sn[2], sn[3], sn[4]
 	);
+
+    package_t* package = package_create_tag(tag64);
+
+    // send package...
+
+    package_free(package);
 }
 
 void app_main(void) {
@@ -82,4 +126,45 @@ void app_main(void) {
         vTaskDelay(35 / portTICK_PERIOD_MS);
     }
 #endif
+}
+
+static device_t* device_create() {
+    return cu_ctor(device_t,
+        .name = strdup("ESP32"),
+        .version = strdup(esp_get_idf_version()),
+        .uptime = esp_timer_get_time(),
+        .heap = esp_get_free_heap_size()
+    );
+}
+
+static package_t* package_create_tag(uint64_t serial_number) {
+    package_t* pckg = cu_ctor(package_t,
+        .type = PACKAGE_TAG,
+        .device = device_create(),
+        .serial_number = malloc(sizeof(uint64_t))
+    );
+    
+    *(pckg->serial_number) = serial_number;
+    
+    return pckg;
+}
+
+static void device_free(device_t* device) {
+    if(!device) {
+        return;
+    }
+
+    free(device->name);
+    free(device->version);
+    free(device);
+}
+
+static void package_free(package_t* package) {
+    if(!package) {
+        return;
+    }
+
+    free(package->serial_number);
+    device_free(package->device);
+    free(package);
 }
